2f5a9f3d5ee7162ccda754c07917b57d7bf19999
Ramya191-prog <112479392+Ramya191-prog@users.noreply.github.com>
2022-09-14 15:06:59 +0530

    Add files via upload

---
+++ Syntax-Identation-Comments.py
@@ -0,0 +1,24 @@
+# Python Syntax:
+print("Hello, World!")
+
+#Python Indentation:
+if 5 > 2:
+  print("Five is greater than two!")
+
+#Python Indentation Syntax error:
+#if 5 > 2:
+#print("Five is greater than two!")
+
+##This is a comment.
+print("Hello, World!")
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Multi Line Comments
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
