35dc9d89522b51aedf15053b07b8e0f1e2a2c05f
Ramya191-prog <112479392+Ramya191-prog@users.noreply.github.com>
2022-09-15 10:23:25 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,60 @@
+#Getting the Data Type with type() function
+x = 5
+print(type(x))
+
+#Data types
+#Data Type = str
+x = "Hello World"   
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = int
+x = 20  
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = float
+x = 20.5   
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = complex
+x = 3j  
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = list
+x = ["apple", "banana", "cherry"]  
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+
+#Data Type = tuple
+x = ("apple", "banana", "cherry")  
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = range
+x = range(6) 
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = dict
+x = {"name" : "John", "age" : 36} 
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = set
+x = {"apple", "banana", "cherry"}
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = frozenset
+x = frozenset({"apple", "banana", "cherry"}) 
+print(x)            #display x:
+print(type(x))      #display the data type of x:
+
+#Data Type = bool
+x =True 
+print(x)            #display x:
+print(type(x))      #display the data type of x:
